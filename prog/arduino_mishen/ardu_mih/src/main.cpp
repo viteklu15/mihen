b54f48aef2555a72ff9f5e2542ccfdd0479c2589
viteklu15 <viteklu15@users.noreply.github.com>
2019-02-25 21:33:58 +0700

    почти сделал мишень программу

--- prog/arduino_mishen/ardu_mih/src/main.cpp
+++ prog/arduino_mishen/ardu_mih/src/main.cpp
@@ -2,9 +2,18 @@
 
 const int wakeUpPin = 2;
 
-bool flag_prer = false;
 int skolko_rabotat = 5; //в секундах
 int kol_sek = 0;        //в секундах
+
+int analog_sens_ciramik = 3; //pc3
+int val_sens_ciramik, bat = 0;
+int analog_bat = 2; //pc2
+int out_led = 13;   // выход для светодиода
+int bit_0 = 7;
+int bit_1 = 8;
+int bit_2 = 9;
+String M_bit = "";
+String Z_bit = "";
 
 bool on_off = false;
 String riad_serial = "";
@@ -20,29 +29,41 @@ void loop2()
       char inChar = Serial.read();
       riad_serial += inChar;
       if (inChar == '\n')
-      {       
-        if (riad_serial.indexOf("M1") != -1)
+      {
+        if (riad_serial == M_bit + "1\n")
         {
-          String Voltage = riad_serial.substring(2);
-          Voltage.trim();
-          int on_M = Voltage.toInt();
-          if (on_M == 1)
-          {
-            on_off = true;
-            Serial.print("M11\n ");
-          }
-          else
-          {
-            on_off = false;
-            //Serial.println("false");
-            Serial.print("M10\n ");
-          }
+          on_off = true;
+          Serial.print(M_bit + 1 + "\n");
         }
-        riad_serial = "";
+        else
+        {
+          on_off = false;
+          Serial.print(M_bit + 0 + "\n");
+        }
+
+        riad_serial = ""; // обнуляем переменную стринг 
+        bat = analogRead(analog_bat);
+        bat = map(bat, 0, 1023, 0, 100);
+        Serial.print(Z_bit);
+        Serial.print(bat);
+        Serial.print("\n");
       }
     }
+    if (on_off)
+    {
+      digitalWrite(out_led, HIGH);
+      val_sens_ciramik = analogRead(analog_sens_ciramik);
+      if (val_sens_ciramik > 300)
+      {
+        Serial.print(M_bit + val_sens_ciramik + "\n"); // передаем данные сенсора
+      }
+    }
+    else
+    {
+      digitalWrite(out_led, LOW);
+    }
   }
-  if ((millis() - tamer_sec) > 1000)
+  if (((millis() - tamer_sec) > 1000) & !on_off)
   {
     tamer_sec = millis();
     kol_sek++;
@@ -56,10 +77,40 @@ void wakeUp()
 
 void setup()
 {
-  pinMode(13, OUTPUT);
+  pinMode(out_led, OUTPUT);
   pinMode(wakeUpPin, INPUT);
+  pinMode(bit_0, INPUT_PULLUP);
+  pinMode(bit_1, INPUT_PULLUP);
+  pinMode(bit_2, INPUT_PULLUP);
   Serial.begin(9600);
   detachInterrupt(0);
+
+  if ((digitalRead(bit_2) == 0) & (digitalRead(bit_1) == 1) & (digitalRead(bit_0) == 0))
+  {
+    M_bit = "M5";
+    Z_bit = "Z5";
+  }
+  else if ((digitalRead(bit_2) == 0) & (digitalRead(bit_1) == 1) & (digitalRead(bit_0) == 1))
+  {
+    M_bit = "M4";
+    Z_bit = "Z4";
+  }
+  else if ((digitalRead(bit_2) == 1) & (digitalRead(bit_1) == 0) & (digitalRead(bit_0) == 0))
+  {
+    M_bit = "M3";
+    Z_bit = "Z3";
+  }
+  else if ((digitalRead(bit_2) == 1) & (digitalRead(bit_1) == 0) & (digitalRead(bit_0) == 1))
+  {
+    M_bit = "M2";
+    Z_bit = "Z2";
+  }
+  else if ((digitalRead(bit_2) == 1) & (digitalRead(bit_1) == 1) & (digitalRead(bit_0) == 1))
+  {
+    M_bit = "M1";
+    Z_bit = "Z1";
+  }
+  Serial.print(M_bit);
 }
 
 void loop()
@@ -72,7 +123,7 @@ void loop()
   else
   {
     Serial.print("Sleep \n ");
-  // Serial.println(10, HEX);
+    // Serial.println(10, HEX);
     delay(1000);
     //sleepMode(SLEEP_POWER_DOWN);
     //sleep(); // Go to sleep
